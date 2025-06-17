ce26e56f8cd03b4e46042f762e8344d92bb8ee04
Rodrigo Cisneros <cmjrodrigo53@gmail.com>
2025-06-16 20:50:44 -0600

    First version :) (Forgot to update make.sh)

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 g++ ./source/Entity.cpp source/WindowManager.cpp ./source/Block.cpp ./source/Player.cpp ./source/Projectile.cpp ./source/Alien.cpp ./source/GameManager.cpp -c -fsanitize=address
-g++ main.cpp Entity.o WindowManager.o Block.o Player.o Projectile.o Alien.o GameManager.o -lX11 -fsanitize=address
+g++ main.cpp Entity.o WindowManager.o Block.o Player.o Projectile.o Alien.o GameManager.o -lX11 -fsanitize=address -o Game.out
 mv *.o ./objects
